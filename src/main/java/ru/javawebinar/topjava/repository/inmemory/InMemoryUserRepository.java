c52bef4cee4bb09186a165e2820e936e767f7594
mavic147 <vamaximenko104@gmail.com>
2021-02-12 17:44:32 +0300

    InMemoryUserRepository implemented

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,61 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    public static Map<Integer, User> userRepo = new ConcurrentHashMap<>();
+    private final int id = SecurityUtil.authUserId();
+
+    {
+        userRepo.put(id, new User(id, "Peter", "peter@gmail.com", "peter123", Role.USER));
+        userRepo.put(id, new User(id, "Anna", "anna@gmail.com", "anna123", Role.USER));
+        userRepo.put(id, new User(id, "Nicholas", "nic@gmail.com", "nic123", Role.USER));
+        userRepo.put(id, new User(id, "Clementine", "clem@gmail.com", "clem123", Role.USER));
+        userRepo.put(id, new User(id, "Sean", "sean@gmail.com", "sean123", Role.USER));
+        userRepo.put(id, new User(id, "Samantha", "samantha@gmail.com", "sam123", Role.USER));
+    }
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return userRepo.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
+        userRepo.put(id, user);
         return user;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return userRepo.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return userRepo.entrySet().stream().map(userRepo::get).sorted(Comparator.comparing(AbstractNamedEntity::getName))
+                .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return email != null ? userRepo.entrySet().stream().filter(user -> user.getValue().getEmail().equals(email))
+                .findFirst().get().getValue() : null ;
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
+//Info from authorized user
 public class SecurityUtil {
 
     public static int authUserId() {
