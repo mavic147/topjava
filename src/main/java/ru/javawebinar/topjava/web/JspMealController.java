8db10be6fa8ab00a510c7d146ba05c24c45fd89f
mavic147 <vamaximenko104@gmail.com>
2021-03-24 21:09:30 +0300

    getFilteredMeals method added

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @Controller
 public class JspMealController {
@@ -17,6 +24,17 @@ public class JspMealController {
     public String getAllMeals(Model model) {
         int userId = SecurityUtil.authUserId();
         model.addAttribute("meals", MealsUtil.getTos(mealService.getAll(userId), SecurityUtil.authUserCaloriesPerDay()));
+        return "meals";
+    }
+
+    @GetMapping("/meals")
+    public String getAllFiltered(@RequestParam(value = "startDate", required = false) LocalDate startDate,
+                                 @RequestParam(value = "endDate", required = false) LocalDate endDate,
+                                 @RequestParam(value = "startTime", required = false) LocalTime startTime,
+                                 @RequestParam(value = "endTime", required = false) LocalTime endTime, Model model) {
+        int userId = SecurityUtil.authUserId();
+        List<Meal> mealsDateFiltered = mealService.getBetweenInclusive(startDate, endDate, userId);
+        model.addAttribute("meals", MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime ,endTime));
         return "meals";
     }
 
