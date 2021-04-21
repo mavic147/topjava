024f9940c3f87c08b54557ee81cc58c6572daebe
mavic147 <vamaximenko104@gmail.com>
2021-04-21 15:15:23 +0300

    response reading in root controller

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -5,14 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 @Controller
 public class RootController {
@@ -31,9 +37,18 @@ public class RootController {
     public String getUsers(Model model) throws IOException {
 //        model.addAttribute("users", userService.getAll());
         URL url = new URL("http://localhost:8081/topjava/users");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        List<User> userList = JsonUtil.readValues(content.toString(), User.class);
+        in.close();
+        model.addAttribute("users", userList);
         return "users";
     }
 
@@ -49,9 +64,17 @@ public class RootController {
 //        model.addAttribute("meals",
 //                MealsUtil.getTos(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
         URL url = new URL("http://localhost:8081/topjava/meals");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        List<MealTo> mealsTo = JsonUtil.readValues(content.toString(), MealTo.class);
+        model.addAttribute("meals", mealsTo);
         return "meals";
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -11,8 +11,8 @@
         <form method="post" action="users" class="form-inline">
             <label><spring:message code="app.login"/></label>
             <select name="userId" class="form-control mx-3">
-                <option value="100000" selected>User</option>
-                <option value="100001">Admin</option>
+                <option value="6080108a7b5228092ac51dd6" selected>User</option>
+                <option value="6080108b7b5228092ac51dd7">Admin</option>
             </select>
             <button type="submit" class="btn btn-primary"><spring:message code="common.select"/></button>
         </form>
