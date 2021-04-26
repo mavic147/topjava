b9346a5e7d76a7e3ae508b09d8026428703793e9
mavic147 <vamaximenko104@gmail.com>
2021-04-26 09:53:20 +0300

    hot fix

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -6,12 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -37,25 +31,5 @@ public class AdminUIController extends AbstractUserController {
                        @RequestParam String email,
                        @RequestParam String password) {
         super.create(new User(null, name, email, password, Role.USER));
-//        URL url = new URL("http://localhost:8081/topjava/users/");
-//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//        connection.setRequestMethod("POST");
-//        connection.setRequestProperty("Content-Type", "application/json; utf-8");
-//        connection.setDoOutput(true);
-//        String jsonInputString = String.format("{\"name\":\"%s\", \"email\":\"%s\", \"password\":\"%s\"}",
-//                name, email, password);
-//        try(OutputStream os = connection.getOutputStream()) {
-//            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
-//            os.write(input, 0, input.length);
-//        }
-//        BufferedReader in = new BufferedReader(
-//                new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-//        String inputLine;
-//        StringBuilder content = new StringBuilder();
-//        while ((inputLine = in.readLine()) != null) {
-//            content.append(inputLine);
-//        }
-//        in.close();
-//        return "redirect:users";
     }
 }
